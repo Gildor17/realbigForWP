9518473da6d030a1c445f14310421c2f5d8f399e
Serhii Shkurenko <furioous357@gmail.com>
2018-08-15 15:23:47 +0300

    version up

--- realbigForWP.php
+++ realbigForWP.php
@@ -15,7 +15,7 @@ include ( ABSPATH . "wp-content/plugins/realbigForWP/synchronising.php");
 /*
 Plugin name:  Realbig For WordPress
 Description:  Реалбиговский плагин для вордпреса. Для полного описания перейдите по ссылке: <a href="https://github.com/Gildor17/realbigFoWP/blob/master/README.MD" target="_blank">https://github.com/Gildor17/realbigFoWP/blob/master/README.MD</a>
-Version:      0.1.15a
+Version:      0.1.16a
 Author:       Gildor
 License:      GPL2
 License URI:  https://www.gnu.org/licenses/gpl-2.0.html
@@ -34,7 +34,7 @@ try
 		'realbigForWP'
 	);
 	/****************** end of updater code *******************************************************************************/
-	$GLOBALS['realbigForWP_version'] = '0.1.15a';
+	$GLOBALS['realbigForWP_version'] = '0.1.16a';
 	/********** checking and creating tables ******************************************************************************/
 	$wpPrefix = $wpdb->base_prefix;
 	if ( empty( $wpPrefix ) ) {
