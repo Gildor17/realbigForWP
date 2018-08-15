789ca64f6c52491dfcd63912ffc06c4dc9687480
Serhii Shkurenko <furioous357@gmail.com>
2018-08-15 12:48:38 +0300

    fix

--- realbigForWP.php
+++ realbigForWP.php
@@ -81,7 +81,7 @@ if (!empty($token)&&$token!='no token')
 		    {
 			    $sameTokenResult = true;
 			    synchronize($token, $wpOptionsCheckerSyncTime, $sameTokenResult, $wpPrefix);
-			    tokenTimeUpdateChecking($GLOBALS['token']);
+			    tokenTimeUpdateChecking($GLOBALS['token'], $wpPrefix);
 		    }
         }
     }
@@ -111,118 +111,7 @@ if (strpos($GLOBALS['PHP_SELF'], 'wp-admin') != false)
 	if (!empty($_POST['tokenInput']))
 	{
 		$sameTokenResult = false;
-		synchronize($_POST['tokenInput'], (empty($wpOptionsCheckerSyncTime) ? NULL : $wpOptionsCheckerSyncTime), $sameTokenResult);
-//    if (!empty($token)&&$_POST['tokenInput']==$token&&$blocksSettingsTableChecking!=0)
-//    {
-//	    $sameTokenResult = true;
-//    }
-//    else
-//    {
-//	    $sameTokenResult = false;
-//    }
-////	$url = 'http://realbigweb/api/wp-get-settings?token='.$_POST['tokenInput'];     // orig web get
-////	$url = 'http://realbigweb/api/wp-get-settings';     // orig web post
-//	$url = 'https://realbig.media/api/wp-get-settings?token='.$_POST['tokenInput'];     // orig
-//
-//    $dataForSending = [
-//        'token' => $_POST['tokenInput']
-//    ];
-//
-//	try
-//	{
-//		$ch = curl_init('https://realbig.media/api/wp-get-settings?token='.$_POST["tokenInput"]);
-////	    curl_setopt($ch, CURLOPT_URL, $url);
-////		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.6 (KHTML, like Gecko) Chrome/16.0.897.0 Safari/535.6');
-//		curl_setopt($ch, CURLOPT_POST, 1);
-//		curl_setopt($ch, CURLOPT_POSTFIELDS, $dataForSending);
-//		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//		curl_setopt($ch, CURLOPT_TIMEOUT, 15);
-//		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-//		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
-//		curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
-//	    curl_setopt($ch, CURLOPT_COOKIE, '');
-//	    curl_setopt($ch, CURLOPT_COOKIEFILE, '');
-//		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-//		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-////	    curl_setopt($ch, CURLOPT_REFERER, $url);
-////		$connectionChecker = curl_getinfo($ch, CURLINFO_OS_ERRNO);
-//		$jsonToken = curl_exec($ch);
-//		curl_close($ch);
-//
-////		echo '<script>console.log("'.$jsonToken.'")</script>';
-//
-//		if (!empty($jsonToken))
-//		{
-//			$GLOBALS['connection_request_rezult'] = 1;
-//			$GLOBALS['connection_request_rezult_1'] = 'success';
-//		}
-//		else
-//		{
-//			$error = error_get_last();
-//			$GLOBALS['connection_request_rezult'] = 'Connection error: ' . $error['message'];
-//			$GLOBALS['connection_request_rezult_1'] = 'Connection error: ' . $error['message'];
-//
-//        }
-//	}
-//	catch (Exception $e)
-//	{
-//		$GLOBALS['tokenStatusMessage'] = $e['message'];
-//	}
-//
-//	if (!empty($jsonToken))
-//	{
-//		$decodedToken = json_decode($jsonToken, true);
-//		$GLOBALS['tokenStatusMessage'] = $decodedToken['message'];
-//    }
-//    else
-//    {
-//	    $decodedToken = NULL;
-//	    $GLOBALS['tokenStatusMessage'] = 'ошибка соединения';
-//    }
-//
-//    if (!empty($decodedToken['data']))
-//    {
-//        try
-//        {
-//	        $counter = 0;
-//	        $wpdb->query($wpdb->prepare( 'DELETE FROM '.$wpPrefix.'realbig_plugin_settings', []));
-//	        $sqlTokenSave = "INSERT INTO ".$wpPrefix."realbig_plugin_settings (text, block_number, setting_type, element, directElement, elementPosition, elementPlace, firstPlace, elementCount, elementStep) VALUES ";
-//
-//	        foreach ($decodedToken['data'] AS $k => $item)
-//	        {
-//		        $counter++;
-//		        $sqlTokenSave .= ( $counter != 1 ? ", " : "") . "('" . $item['text'] . "', " . (int)$item['block_number'] . ", " . (int)$item['setting_type'] . ", '" . htmlspecialchars($item['element']) . "', '" . htmlspecialchars($item['directElement']) . "', " . (int)$item['elementPosition'] . ", " . (int)$item['elementPlace'] . ", " . (int)$item['firstPlace'] . ", " . (int)$item['elementCount'] . ", " . (int)$item['elementStep'] . ")";
-//	        }
-//	        $sqlTokenSave .= " ON DUPLICATE KEY UPDATE text = values(text), setting_type = values(setting_type), element = values(element), directElement = values(directElement), elementPosition = values(elementPosition), elementPlace = values(elementPlace), firstPlace = values(firstPlace), elementCount = values(elementCount), elementStep = values(elementStep) ";
-//
-//		    $wpdb->query($wpdb->prepare($sqlTokenSave, []));
-//		    // if no needly note, then create
-//		    $wpOptionsCheckerTokenValue = $wpdb->query($wpdb->prepare( "SELECT optionValue FROM " . $wpPrefix . "realbig_settings WHERE optionName = '_wpRealbigPluginToken'", []));
-//		    if (empty($wpOptionsCheckerTokenValue))
-//		    {
-//			    $wpdb->insert($wpPrefix.'realbig_settings', ['optionName'=>'_wpRealbigPluginToken', 'optionValue'=>$_POST["tokenInput"]]);
-//		    }
-//		    else
-//		    {
-//			    $wpdb->update($wpPrefix.'realbig_settings', ['optionName'=>'_wpRealbigPluginToken', 'optionValue'=>$_POST["tokenInput"]], ['optionName'=>'_wpRealbigPluginToken']);
-//		    }
-//
-////            $wpOptionsCheckerSyncTime = $wpdb->get_row($wpdb->prepare('SELECT optionValue FROM '.$wpPrefix.'realbig_settings WHERE optionName = "token_sync_time"', []));
-//		    if (empty($wpOptionsCheckerSyncTime)) {
-//			    $wpdb->insert($wpPrefix.'realbig_settings', ['optionName'=>'token_sync_time', 'optionValue'=> time()]);
-//		    }
-//		    else
-//		    {
-//			    $wpdb->update($wpPrefix.'realbig_settings', ['optionName'=>'token_sync_time', 'optionValue'=> time()], ['optionName'=>'token_sync_time']);
-//		    }
-//
-//		    $GLOBALS['token'] = $_POST["tokenInput"];
-//	    }
-//	    catch (Exception $e)
-//	    {
-//		    $GLOBALS['tokenStatusMessage'] = $e;
-//	    }
-//    }
+		synchronize($_POST['tokenInput'], (empty($wpOptionsCheckerSyncTime) ? NULL : $wpOptionsCheckerSyncTime), $sameTokenResult, $wpPrefix);
 	}
     elseif ($GLOBALS['token'] == 'no token')
 	{
