fc292406a7b3ecff25137462e04718b909428539
Serhii Shkurenko <furioous357@gmail.com>
2018-09-05 17:40:10 +0300

    fix

--- synchronizationJS.js
+++ synchronizationJS.js
@@ -3,7 +3,7 @@ syncFunction();
 function syncFunction()
 {
     jQuery.ajax({
-        url: "//"+document.domain+"/wordpress_test/wp-content/plugins/realbigForWP/synchronising.php",
+        url: "//"+document.domain+"/wp-content/plugins/realbigForWP/synchronising.php",
         data:{funcActivator: 'ready'}, //for requesting in file 1
         async: true,
         type: 'post',
