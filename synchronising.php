563c5fdcbfb1ac89ce5abb7a279c781c5378b018
Serhii Shkurenko <furioous357@gmail.com>
2018-11-06 12:42:36 +0200

    fix for table problem

--- synchronising.php
+++ synchronising.php
@@ -11,7 +11,6 @@ include_once ( dirname(__FILE__)."/../../../wp-admin/includes/plugin.php" );
 include_once ( dirname(__FILE__)."/../../../wp-admin/includes/upgrade.php" );
 include_once ( dirname(__FILE__).'/../../../wp-includes/wp-db.php');
 
-
 try {
 	global $wpdb;
 
---
+++ uninstall.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: furio
+ * Date: 2018-11-06
+ * Time: 11:10
+ */
+
+include_once ( dirname(__FILE__).'/../../../wp-load.php' );
+include_once ( dirname(__FILE__)."/../../../wp-admin/includes/plugin.php" );
+include_once ( dirname(__FILE__)."/../../../wp-admin/includes/upgrade.php" );
+include_once ( dirname(__FILE__).'/../../../wp-includes/wp-db.php');
+
+if(defined('WP_UNINSTALL_PLUGIN') ) {
+	global $wpdb;
+
+	$wpPrefix = $table_prefix;
+	if ( empty( $wpPrefix ) ) {
+		$wpPrefix = $wpdb->base_prefix;
+	}
+	$GLOBALS['wpPrefix'] = $wpPrefix;
+
+	delete_option( 'realbig_status_gatherer' );
+	delete_option( 'realbig_status_gatherer_version' );
+
+	$tableName = $wpPrefix . 'realbig_plugin_settings';
+	$wpdb->query("DROP TABLE IF EXISTS ". $tableName);
+	$tableName = $wpPrefix . 'realbig_settings';
+	$wpdb->query("DROP TABLE IF EXISTS ". $tableName);
+}
