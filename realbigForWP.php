9993a1dbe6c699103b569bdce711bdd7d2438ae5
Serhii Shkurenko <furioous357@gmail.com>
2019-02-12 14:59:51 +0200

    version fix

--- realbigForWP.php
+++ realbigForWP.php
@@ -17,7 +17,7 @@ include ( dirname(__FILE__)."/textEditing.php");
 /*
 Plugin name:  Realbig Media
 Description:  Плагин для монетизации от RealBig.media
-Version:      0.1.26.28
+Version:      0.1.26.27
 Author:       Realbig Team
 License:      GPLv2 or later
 License URI:  https://www.gnu.org/licenses/gpl-2.0.html
@@ -77,7 +77,7 @@ try {
 	if ( ! empty( $pluginData['Version'] ) ) {
 		$GLOBALS['realbigForWP_version'] = $pluginData['Version'];
 	} else {
-		$GLOBALS['realbigForWP_version'] = '0.1.26.28';
+		$GLOBALS['realbigForWP_version'] = '0.1.26.27';
 	}
 	$lastSuccessVersionGatherer = get_option( 'realbig_status_gatherer_version' );
 //	require_once( 'synchronising.php' );
