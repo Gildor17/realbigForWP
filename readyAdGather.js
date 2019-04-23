78632f89e17e942083ff8764037a0b6462fd23fe
Serhii Shkurenko <furioous357@gmail.com>
2019-04-23 11:45:02 +0300

    updating and errors fixing in caching system; other fixes.

--- readyAdGather.js
+++ readyAdGather.js
@@ -1,36 +1,105 @@
 var nReadyBlock = false;
-// var nReadyBlock = true;
-// var penyok_stoparik = 0;
+var fetchedCounter = 0;
 
 function sendReadyBlocksNew(blocks) {
+    let decodedData;
     let xhttp = new XMLHttpRequest();
-    let sendData = 'type=blocksGethering&data='+blocks;
-    xhttp.onreadystatechange = function() {
+    let sendData = 'action=saveAdBlocks&type=blocksGethering&data='+blocks;
+    xhttp.onreadystatechange = function(redata) {
         if (this.readyState == 4 && this.status == 200) {
+            if (redata) {
+                // decodedData = JSON.parse(redata);
+            }
+
             console.log('cache succeed');
             // document.getElementById("demo").innerHTML = this.responseText;
         }
     };
-    xhttp.open("POST", ajaxurl, true);
+    // xhttp.open("POST", ajaxurl, true);
+    xhttp.open("POST", adg_object.ajax_url, true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhttp.send(sendData);
 }
 
-function gatherReadyBlocks() {
+function gatherReadyBlocksOrig() {
     let dottaCounter = 0;
     let blocks = '';
     let gatheredBlocks = document.getElementsByClassName('content_rb');
+    let checker = 0;
+    let adContent = '';
+    let curState = '';
 
     if (gatheredBlocks.length > 0) {
-        blocks += '{"data":[';
+        // blocks += '{"data":[';
+        blocks += '{data:[';
         for (let i = 0; i < gatheredBlocks.length; i++) {
-            if (dottaCounter > 0) {
-                blocks += ',';
+            curState = gatheredBlocks[i]['dataset']["state"].toLowerCase();
+            checker = 0;
+            if (curState&&(gatheredBlocks[i]['innerHTML'].length > 0||curState=='no-block')) {
+                if (gatheredBlocks[i]['innerHTML'].length > 0) {
+                    checker = 1;
+                    adContent = gatheredBlocks[i]['innerHTML'].replace(/\"/g, "\'");
+                } else if (curState=='no-block') {
+                    checker = 1;
+                    adContent = '';
+                }
+                if (checker==1) {
+                    if (dottaCounter > 0) {
+                        blocks += ',';
+                    }
+                    // blocks += '{"id":"'+gatheredBlocks[i]['dataset']['id']+'","code":"'+adContent+'"}';
+                    blocks += '{id:'+gatheredBlocks[i]['dataset']['id']+',code:"'+adContent+'"}';
+                    dottaCounter++;
+                }
             }
-            blocks += '{"id":"'+gatheredBlocks[i]['dataset']['id']+'","code":"'+gatheredBlocks[i]['innerHTML'].replace(/\"/g, "\'")+'"}';
-            dottaCounter++;
         }
-        blocks += "]}";
+        blocks += ']}';
+
+        let newBlocks = JSON.stringify(blocks);
+
+        sendReadyBlocksNew(blocks);
+    }
+}
+
+function gatherReadyBlocks() {
+    // let blocks = '';
+    let blocks = {};
+    let counter1 = 0;
+    let gatheredBlocks = document.getElementsByClassName('content_rb');
+    let checker = 0;
+    let adContent = '';
+    let curState = '';
+    let thisData = [];
+    let sumData = [];
+    let newBlocks = '';
+    let thisDataString = '';
+
+    if (gatheredBlocks.length > 0) {
+        blocks.data = {};
+
+        for (let i = 0; i < gatheredBlocks.length; i++) {
+            curState = gatheredBlocks[i]['dataset']["state"].toLowerCase();
+            checker = 0;
+            if (curState&&(gatheredBlocks[i]['innerHTML'].length > 0||curState=='no-block')) {
+                if (gatheredBlocks[i]['innerHTML'].length > 0) {
+                    checker = 1;
+                    adContent = gatheredBlocks[i]['innerHTML'];
+                    // adContent = decodeURIComponent(adContent);
+                    adContent = adContent.replace(/&amp;/g, 'a_m_p');
+                    adContent = adContent.replace(/&quot;/g, '"');
+                    adContent = adContent.replace(/\"/g, "\'");
+                } else if (curState=='no-block') {
+                    checker = 1;
+                    adContent = '';
+                }
+                if (checker==1) {
+                    blocks.data[counter1] = {id:gatheredBlocks[i]['dataset']['id'],code:adContent};
+                    counter1++;
+                }
+            }
+        }
+
+        blocks = JSON.stringify(blocks);
 
         sendReadyBlocksNew(blocks);
     }
@@ -38,10 +107,23 @@ function gatherReadyBlocks() {
 
 function timeBeforeGathering() {
     let gatheredBlocks = document.getElementsByClassName('content_rb');
+    let okStates = ['done','refresh-wait','no-block','fetched'];
+    let curState = '';
+
     for (let i = 0; i < gatheredBlocks.length; i++) {
         if (!gatheredBlocks[i]['dataset']["state"]) {
             nReadyBlock = true;
             break;
+        } else {
+            curState = gatheredBlocks[i]['dataset']["state"].toLowerCase();
+            if (!okStates.includes(curState)) {
+                nReadyBlock = true;
+                break;
+            } else if (curState=='fetched'&&fetchedCounter < 3) {
+                fetchedCounter++;
+                nReadyBlock = true;
+                break;
+            }
         }
     }
     if (nReadyBlock == true) {
@@ -57,4 +139,3 @@ if (document.readyState === "complete" || (document.readyState !== "loading" && 
 } else {
     document.addEventListener("DOMContentLoaded", timeBeforeGathering, false);
 }
-
